e281a4f699e85b8b2dcc0ac48427a4f5656e7259
Nyoko <nataliestricker2002@gmail.com>
2022-05-14 17:27:33 +0200

    #70 URS-003 - Fragefeld zum Beschwerdegrad
    URS-003 - Fragefeld zum Beschwerdegrad
    
    Geändert:
    - const für die slider auswahl
    - 2 divs für die jeweilige frage
    - toggle_visibility function placeholder ( -> hab damit versucht zwischen den beiden divs zu wechseln, aber ich checks nicht help )

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import React, { useState } from "react";
 import notificationLogo from "./assets/notify.png";
 
 function App() {
+
+  const [rangeval, setRangeval] = useState(null);
+
   return (
     <>
       <h1>Healthbeat</h1>
       <button onClick={notifyMe}>Notify me!</button>
+      <div id="id1">
+        <p>Wo hast du heute Beschwerden?</p>
+        <input type="radio" value="Rücken" name="pain" /> Rücken <br/>
+        <input type="radio" value="Nacken" name="pain" /> Nacken <br/>
+        <input type="radio" value="Schulter" name="pain" /> Schulter <br/>
+        <input type="radio" value="Hüfte" name="pain" /> Hüfte <br/>
+        <input type="radio" value="Keine" name="pain" /> Keine <br/><br/>
+        <button onclick={toggle_visibility()}>Aktualisieren und Speichern</button>
+      </div>
+      <div id="id2">
+        <p>Wie stark sind deine Schmerzen?</p>
+        <input type="range" className="custom-range" min="0" max="100" 
+          onChange={(event) => setRangeval(event.target.value)} />
+        <p>Dein Schmerzgrad liegt bei: {rangeval}%</p>
+
+        <button onclick={toggle_visibility()}>Zurück</button>
+      </div>
     </>
   );
 }
@@ -22,6 +43,9 @@ document.addEventListener('DOMContentLoaded', function() {
    Notification.requestPermission();
  });
  
+ function toggle_visibility(id1, id2) {
+   
+} 
  
 function notifyMe() {
   if (Notification.permission !== 'granted') {
