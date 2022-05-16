dbe71430347faff258118548f0fb5acabb8bc72d
berkay <yurdaguel.b@gmail.com>
2022-05-16 14:01:02 +0200

    # Global styles
    hinzugefügt:
    - root-wrapper.js
    
    geändert:
    - index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { GlobalStyle } from "root-wrapper"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
